a57109f2542e1e1e64821fd2dcea2b6acc164488
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-09-04 21:58:41 +1200

    commit tree 1b11774e9f6161e4cc201aff718645332321fb19

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
 def starts_with_a_vowel?(word)
-
+    word.match(/^[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.match?(/^[A-Z].*[[:punct:]]$/)
 end
 
-def valid_phone_number?(phone)
-
-end
+def valid_phone_number?(phone_number)
+    # Define a regular expression pattern for a valid phone number
+    pattern = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\d{3}\)|\d{3})(?:\s*(?:[.-]\s*)?)\d{3}(?:\s*(?:[.-]\s*)?)\d{4}|[2-9]\d{2}(?:\s*(?:[.-]\s*)?)\d{3}(?:\s*(?:[.-]\s*)?)\d{4})$/
+    
+    # Use the pattern to check if the phone number matches
+    phone_number.match?(pattern)
+  end
